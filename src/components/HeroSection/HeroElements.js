a006a5074a9df8310623e521114b3a1e539261b2
Devlin Macpherson <71355015+devlinmacpherson@users.noreply.github.com>
2022-01-13 15:21:07 -0500

    save

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {MdKeyboardArrowRight, MdArrowForward} from 'react-icons/md';
 
 export const HeroContainer = styled.div`
   background: whitesmoke;
@@ -21,3 +22,60 @@ export const HeroBg = styled.div`
   height: 100%;
   overflow: hidden;
 `;
+
+export const HeroContent = styled.div`
+z-index: 3;
+max-width: 1200px;
+position: absolute;
+padding: 8px 24px;
+display: flex;
+flex-direction: column;
+align-items: center;
+
+`
+
+export const HeroH1 = styled.h1`
+  color: black;
+  font-size: 48px;
+  text-align: left;
+
+  @media screen and (max-width: 768px){
+    font-size:40px;
+  }
+
+  @media screen and (max-width: 480px){
+    font-size: 32px;
+  }
+`
+export const HeroP = styled.p`
+  margin-top: 24px;
+  color: black;
+  font-size: 18px;
+  text-align: left;
+  max-width: 600px;
+
+  @media screen and (max-width: 768px){
+    font-size:18px;
+  }
+
+  @media screen and (max-width: 480px){
+    font-size: 14px;
+  }
+`
+
+export const HeroBtnWrapper = styled.div`
+  margin-top: 32px;
+  display: flex;
+  flex-direction: column;
+  align-items:left;
+`
+
+export const ArrowForward = styled(MdArrowForward)`
+  margin-left: 8px;
+  font-size 20px;
+`
+
+export const ArrowRight = styled(MdKeyboardArrowRight)`
+  margin-left: 8px;
+  font-size 20px;
+`
--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import BackgroundImage from "../../images/background.png";
-import { HeroContainer, HeroBg, ImageBg } from "./HeroElements";
+import { Button } from "../ButtonElement";
+import { HeroContainer, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight} from "./HeroElements";
 const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  }
+
   return (
     <>
       <HeroContainer style={{ backgroundImage: `url(${BackgroundImage})` }}>
-        <HeroBg></HeroBg>
+        <HeroContent>
+          <HeroH1>Devicist Design Works</HeroH1>
+          <HeroP>Engineering custom hardware and software solutions for creative agencies and exhibit design houses. Specializing in rapid development of interactive installations and custom devices for trade show experiences. </HeroP>
+          <HeroBtnWrapper>
+            <Button to="contact" onMouseEnter={onHover}
+            onMouseLeave={onHover}>
+              Contact Us
+            </Button>
+          </HeroBtnWrapper>
+        </HeroContent>
       </HeroContainer>
     </>
   );

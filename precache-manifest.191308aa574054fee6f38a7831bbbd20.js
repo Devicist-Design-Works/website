d2e78494a51bea4d6eec770493bb6dc9906109a6
devlinmacpherson <dev.macpherson@gmail.com>
2022-06-22 15:22:00 -0400

    Updates

---
+++ precache-manifest.191308aa574054fee6f38a7831bbbd20.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da7a16d0b69c9be5e066cf2a9510d7c",
+    "url": "/devicist/index.html"
+  },
+  {
+    "revision": "6de989baa524270d8fb8",
+    "url": "/devicist/static/css/main.982a61a8.chunk.css"
+  },
+  {
+    "revision": "fdb9965051af80dd3507",
+    "url": "/devicist/static/js/2.b896751a.chunk.js"
+  },
+  {
+    "revision": "3a3eee750260ab555627e944270db51c",
+    "url": "/devicist/static/js/2.b896751a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6de989baa524270d8fb8",
+    "url": "/devicist/static/js/main.35108169.chunk.js"
+  },
+  {
+    "revision": "274ae911728037f79eb1",
+    "url": "/devicist/static/js/runtime-main.15a6a4f2.js"
+  },
+  {
+    "revision": "cd1479ca06ec91a7c84b8dc931516760",
+    "url": "/devicist/static/media/BuildIcon-white.cd1479ca.svg"
+  },
+  {
+    "revision": "835b723b70be0194860baa5b80834cd1",
+    "url": "/devicist/static/media/DesignIcon-white.835b723b.svg"
+  },
+  {
+    "revision": "761bc67a814f1d1e25d464f7fd2db285",
+    "url": "/devicist/static/media/PrototypeIcon-white.761bc67a.svg"
+  },
+  {
+    "revision": "ed68cc9945f60067667a90b2141952d5",
+    "url": "/devicist/static/media/SupportIcon-white.ed68cc99.svg"
+  },
+  {
+    "revision": "47fbacad3d12cdbfccdc179780b46767",
+    "url": "/devicist/static/media/logo.47fbacad.svg"
+  },
+  {
+    "revision": "2356967d07aef7b3843b346cfd691014",
+    "url": "/devicist/static/media/nfc.2356967d.png"
+  },
+  {
+    "revision": "d4987787a89b579a4dded398a1d64805",
+    "url": "/devicist/static/media/powercube.d4987787.png"
+  },
+  {
+    "revision": "44f52e870a9ea79562688afb3ad96d62",
+    "url": "/devicist/static/media/process-build.44f52e87.png"
+  },
+  {
+    "revision": "63991be1681b6257f7c8d403ed19ae3f",
+    "url": "/devicist/static/media/process-design.63991be1.png"
+  },
+  {
+    "revision": "f090e1773fe8989aeeab5b1a2c066d39",
+    "url": "/devicist/static/media/process-prototype.f090e177.png"
+  },
+  {
+    "revision": "7311181696a78730f1c38d904e354a05",
+    "url": "/devicist/static/media/process-support.73111816.png"
+  },
+  {
+    "revision": "4a503d51b8b2cb5c6fc641aa1b74b771",
+    "url": "/devicist/static/media/snowflake.4a503d51.png"
+  },
+  {
+    "revision": "d7665f4abbf987e3e4626391d5af637f",
+    "url": "/devicist/static/media/videoBg.d7665f4a.mp4"
+  }
+]);

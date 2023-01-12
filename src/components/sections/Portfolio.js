b3e8c960b5e8df6a215561ca4c28dd031616fe1d
devlinmacpherson <dev.macpherson@gmail.com>
2023-01-12 14:00:24 -0500

    Updated documentation

--- src/components/sections/Portfolio.js
+++ src/components/sections/Portfolio.js
@@ -53,7 +53,7 @@ const Portfolio = ({
         height: 1,
       },
       {
-        src: require("./../../assets/images/portfolio/Snowflake/circuit.jpg"),
+        src: require("./../../assets/images/portfolio/Snowflake/snowflake2.png"),
         width: 1,
         height: 1,
       },
@@ -63,7 +63,7 @@ const Portfolio = ({
         height: 1,
       },
       {
-        src: require("./../../assets/images/portfolio/Snowflake/handsOn.jpg"),
+        src: require("./../../assets/images/portfolio/Snowflake/snowflake4.png"),
         width: 1,
         height: 1,
       },
@@ -111,7 +111,7 @@ const Portfolio = ({
   const Project3ModalContent = {
     title: "Brain",
     body: "Interactive model of a brain that responds to user input, displaying various patterns",
-    videoUrl: "https://www.youtube.com/embed/w5rAcHXQWz4",
+    videoUrl: "https://www.youtube.com/embed/A3vX9CjIuiw",
     images: [
       {
         src: require("./../../assets/images/portfolio/brain/brain1.png"),
